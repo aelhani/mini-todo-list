c5d0483e94099cd0489209bd7c0e44f402306843
Abderrazak Elhani <abderrazak.elhani@gmail.com>
2020-02-18 17:23:50 +0100

    Clean to start fresh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
+
+export default App;
